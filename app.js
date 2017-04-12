781984e6b4f9089f9f5f784b301cf835b496fdbc
Hamilton Smith <smithhamilton4@gmail.com>
2017-04-12 19:21:34 -0400

    Added form to add campgrounds

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var campgrounds = [
+		{name: "Salmon Creek", image: "https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg"},
+		{name: "Golden Coast", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
+		{name: "Granite Hill", image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg"},
+];	
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
@@ -8,19 +15,23 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-		{name: "Salmon Creek", image: "https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg"},
-		{name: "Golden Coast", image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg"},
-		{name: "Granite Hill", image: "https://farm9.staticflickr.com/8422/7842069486_c61e4c6025.jpg"},
-	];
+	
 
 	res.render("campgrounds", {campgrounds: campgrounds});
 });
 
+app.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs");
+});
+
 app.post("/campgrounds", function(req, res) {
-	res.send("You hit the post route");
 	//get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
 	//redirect back to campgrounds page
+	res.redirect("/campgrounds");
 }); //this is a different route since it is a POST. Naming is following the REST convention.
 
 app.listen(3000, function() {
